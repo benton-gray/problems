80b00735c487823bea1919894b37093a853978e8
Benton Gray <bg171993@gmail.com>
2020-03-16 16:14:38 -0600

    Adding finished code

--- sock_merchant/sock_merchant.py
+++ sock_merchant/sock_merchant.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-
-def test_print():
-    return "here"
-
+import math
 
 def solve_sock_merchant(n, ar):
-    result = 3
+    bucket_dictionary = {}
+    result = 0
+
+    for val in ar:
+        if val in bucket_dictionary:
+            bucket_dictionary[val].append(val)
+        else:
+            bucket_dictionary[val] = [val]
+    # print(bucket_dictionary)
+
+    for val in bucket_dictionary:
+        result += math.floor((len(bucket_dictionary[val]) / 2))
     return result
 
 
 if __name__ == '__main__':
-    print("here")
+    default_vars = {"n": 9, "ar": [10, 20, 20, 10, 10, 30, 50, 10, 20]}
+    solve_sock_merchant(default_vars.get('n'), default_vars.get('ar'))
+    exit(0)
